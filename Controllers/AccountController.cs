43dae44b2ea5dd88119e59688eb38ec5dacbcb24
shiroe <fenrirnobaku1058@gmail.com>
2024-07-13 10:26:46 +0700

    commit source 2024/07/13

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -84,7 +84,7 @@ namespace LoginAPI.Controllers
             {
                 return BadRequest(result.Errors);
             }
-            return Ok("Your account has been created, you can login");
+            return Ok(new JsonResult(new {title = "Account Created", message= "Your account has been created, you can login" }));
         }
 
         #region Private Helper Method
